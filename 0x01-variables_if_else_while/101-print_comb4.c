9041343af1c7bb1269e12c3d04ac5230e2ab15d0
LANZLOT <lancelotlanz@gmail.com>
2022-07-02 00:42:29 -0700

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,17 +15,20 @@ int main(void)
 	{
 		for (q = p + 1; q <= '9'; q++)
 		{
-			if ((p != d) != q)
+			for (q = p + 1; q <= '9'; q++)
 			{
-				putchar(d);
-				putchar(p);
-				putchar(q);
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
 
-				if (d == '7' && p == '8')
+					if (d == '7' && p == '8')
 					continue;
 
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
