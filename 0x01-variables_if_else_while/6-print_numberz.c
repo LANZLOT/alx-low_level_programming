572a5f9440831cf309ceeda7b6be702441098fdd
LANZLOT <lancelotlanz@gmail.com>
2022-07-01 23:46:54 -0700

    a program that prints all single digit numbers of base 10 star
    ting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -12,7 +10,8 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)				
-			putchar((num % 10) + '0');
+		putchar((num % 10) + '0');
+	
 	putchar('\n');
 
 	return (0);
